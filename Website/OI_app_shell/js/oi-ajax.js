209e164847eb95b8b4e6f0415b8cf52515cd42be
JonathanRys <jonathan.rk.rys@gmail.com>
2016-07-19 08:57:28 -0400

    Added a placeholder file for ajax calls to the backend API.

---
+++ Website/OI_app_shell/js/oi-ajax.js
@@ -0,0 +1,17 @@
+var customAJAX = function(){
+    var me = {};
+
+    me.requestBySpecies = function(binomialName) {
+        //
+    }
+
+    me.requestByCommonName = function(commonName) {
+        //
+    }
+
+    me.identify = function(image) {
+        //
+    }
+
+    return me
+}();
